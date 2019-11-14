6e1a497a0554ba52e23efac2814b3bd2784c850b
Usman Khalid <usmankhalid2591@users.noreply.github.com>
2019-11-14 16:57:17 +0500

    Initial commit

---
+++ js/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":" ","multiple":"{num}","one":"1"}},"counts":[{"id":"78062 https:\/\/www.quantamagazine.org\/?p=78062","comments":22}]});
+}
